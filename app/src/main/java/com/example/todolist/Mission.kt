862456d81156a07292cfefcc17dc44fc11491dab
Nick Hong <zx2581396@gmail.com>
2021-01-28 16:40:51 +0800

    Clean code

--- app/src/main/java/com/example/todolist/Mission.kt
+++ app/src/main/java/com/example/todolist/Mission.kt
@@ -2,22 +2,6 @@ package com.example.todolist
 
 import java.util.*
 
-//sealed class Todo(val viewType: Int) {
-//
-//    data class Title(
-//        val text: String
-//    ) : Todo(TYPE_TITLE)
-//
-//    data class Mission(
-//        val name: String,
-//        val checked: Boolean
-//    ) : Todo(TYPE_MISSION)
-//
-//    companion object {
-//        const val TYPE_TITLE = 0
-//        const val TYPE_MISSION = 1
-//    }
-//}
 data class Mission(
     val id: Int,
     val name: String,
